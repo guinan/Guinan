501b389af52aad70c76e730d48880e5a1b7f3413
kkrieger <kkrieger@5dec2bdd-bc6e-6ca5-d606-a3cd11ca8a0f>
2015-08-27 12:04:49 +0000

    started to implement threading and asynchronous calls

--- Guinan/src/de/ovgu/wdok/guinan/SFPGenJob.java
+++ Guinan/src/de/ovgu/wdok/guinan/SFPGenJob.java
@@ -4,21 +4,25 @@ import java.util.Date;
 
 public class SFPGenJob {
 	
-	private String status;
+	public static final int PROCESSING=1;
+	public static final int FINISHED=2;
+	public static final int GONE=2;
+	
+	private int status;
 	
 	private Date timestamp;
 
-	public SFPGenJob(String status, Date timestamp) {
+	public SFPGenJob(int status, Date timestamp) {
 		super();
 		this.status = status;
 		this.timestamp = timestamp;
 	}
 
-	public String getStatus() {
+	public int getStatus() {
 		return status;
 	}
 
-	public void updateStatus(String status) {
+	public void updateStatus(int status) {
 		this.status = status;
 	}
 
